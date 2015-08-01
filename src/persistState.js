f12c9293f55f811b0240d3cc946a8134ac83fa4a
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-07-31 23:41:59 -0400

    Add hydrate and dehydrate config functions
    - Provide functions to modify data before storing to localStorage
      (dehydrate) or after reading from localStorage and using as the
      store state (hydrate)

--- src/persistState.js
+++ src/persistState.js
@@ -9,13 +9,19 @@ import createSlicer from './createSlicer.js'
  * @param {String} [config.key="redux"] String used as localStorage key
  * @param {Function} [config.slicer] (paths) => (state) => subset. A function that returns a subset
  * of store state that should be persisted to localStorage
+ * @param {Function} [config.dehydrate] (data) => dehydratedData. A function that allows modification of data
+ * before persisting to localStorage
+ * @param {Function} [config.hydrate] (data) => hydratedData. A function that allows modification of data
+ * after reading from localStorage and before setting to a store's state
  *
  * @return {Function} An enhanced store
  */
 export default function persistState(paths, config) {
   const defaults = {
     key: 'redux',
-    slicer: createSlicer
+    slicer: createSlicer,
+    hydrate: data => data,
+    dehydrate: data => data
   }
 
   const cfg = {...defaults, ...config}
@@ -25,7 +31,7 @@ export default function persistState(paths, config) {
     let finalInitialState
 
     try {
-      persistedState = JSON.parse(localStorage.getItem(cfg.key))
+      persistedState = cfg.hydrate(JSON.parse(localStorage.getItem(cfg.key)))
       finalInitialState = persistedState ? {...initialState, ...persistedState} : initialState
     } catch (e) {
       console.warn('Failed to retrieve initialize state from localStorage:', e)
@@ -39,7 +45,7 @@ export default function persistState(paths, config) {
       const subset = slicer(state)
 
       try {
-        localStorage.setItem(cfg.key, JSON.stringify(subset))
+        localStorage.setItem(cfg.key, cfg.dehydrate(JSON.stringify(subset)))
       } catch (e) {
         console.warn('Unable to persist state to localStorage:', e)
       }
